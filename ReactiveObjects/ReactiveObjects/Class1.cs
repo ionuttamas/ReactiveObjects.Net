8f00441634367dff7c6ab3fe1209713b102cbe97
Ionut Tamas <ionut.tamas@tora.com>
2017-09-04 11:54:55 +0300

    Work in progress

---
+++ ReactiveObjects/ReactiveObjects/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveObjects
+{
+    class R {
+        public static Expression Of(Expression<Func<R<object>>> expression) {
+            return expression;
+        }
+    }
+
+    class R<T> {
+
+
+        public T Value { get; }
+
+        public R(T value) {
+            Value = value;
+        }
+
+        public static implicit operator T(R<T> instance) {
+            return instance.Value;
+        }
+
+        public static implicit operator R<T>(T instance) {
+            return new R<T>(instance);
+        }
+
+        public static implicit operator R<T>(Expression expression) {
+
+            return null;
+        }
+    }
+}
